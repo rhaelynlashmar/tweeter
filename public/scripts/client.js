4e0427944bdfccafc31a1cb95662bfd638b61d9d
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-01-05 13:10:00 -0800

    Removed test data object

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,7 +38,7 @@ const createTweetElement = (tweet) => {
 };
 
 
-
+// Loads page before running the following code
 $(document).ready(function() {
   // Event listener for form submission
   $('#new-tweet-form').on('submit', function(event) {
@@ -99,28 +99,5 @@ $(document).ready(function() {
 
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
